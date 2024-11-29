b9f78e66afdcf2a97502fb889cb79f0fb5556141
pankajkoree <pankajkori2057@gmail.com>
2024-11-29 14:42:25 +0530

    dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,10 +4,18 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import teslaIcon from "../../../public/tesla-icon.png";
 import toast from "react-hot-toast";
+import getStarted from "../../../public/getStarted.png";
+import library from "../../../public/library.png";
+import settings from "../../../public/settings.png";
+import activities from "../../../public/activities.png";
+import reports from "../../../public/reports.png";
+import people from "../../../public/people.png";
+import user from "../../../public/user.png";
 
 const Dashboard = () => {
   const [data, setData] = useState(null);
   const [error, setError] = useState<Error | null>(null);
+  const [activeView, setActiveView] = useState<string>("Reports");
 
   // fetching the data using axios
   useEffect(() => {
@@ -28,19 +36,24 @@ const Dashboard = () => {
     fetchData();
   }, []);
 
-  if (error) {
-    return toast.error(error.message);
-  }
-
-  if (data) {
-    return toast.success(data);
-  }
+  const renderContent = () => {
+    switch (activeView) {
+      case "Reports":
+        return <div className="p-4">This is the Reports section.</div>;
+      case "Library":
+        return <div className="p-4">Library section</div>;
+      case "People":
+        return <div className="p-4">People section.</div>;
+      case "Activities":
+        return <div className="p-4">Activities section.</div>;
+    }
+  };
 
   return (
     // dashboard page
-    <div className="relative flex text-gray-600">
+    <div className="relative flex text-gray-600 bg-gray-100">
       {/* side bar */}
-      <div className="relative flex flex-col bg-white xl:w-[230px] xl:min-h-screen">
+      <div className="relative flex flex-col bg-white rounded-r-xl xl:w-[230px] xl:min-h-screen">
         {/* logo div */}
         <div className="relative flex xl:top-[43px] xl:left-[38px]">
           <Image
@@ -52,28 +65,76 @@ const Dashboard = () => {
         {/* end logo div */}
 
         {/* menu buttons */}
-        <div className="relative flex flex-col xl:top-[90px] gap-2">
-          <button className="relative flex items-center justify-center xl:w-[193px] xl:h-[46px] xl:rounded-lg border">
+        <div className="relative flex flex-col xl:top-[90px] gap-2 font-bold justify-center xl:w-full">
+          <button
+            onClick={() => setActiveView("Reports")}
+            className={`relative flex items-center justify-center xl:w-[80%] xl:left-[10%] xl:h-[46px] xl:rounded-lg hover:bg-blue-100 hover:text-blue-400 ${
+              activeView === "Reports" ? "bg-blue-100 text-blue-400" : ""
+            }`}
+          >
+            <Image src={reports} alt="reports" className="xl:w-[24px] mr-2.5" />
             Reports
           </button>
-          <button className="relative flex items-center justify-center xl:w-[193px] xl:h-[46px]  xl:rounded-lg border">
+          <button
+            onClick={() => setActiveView("Library")}
+            className={`relative flex items-center justify-center xl:w-[80%] xl:left-[10%] xl:h-[46px] xl:rounded-lg hover:bg-blue-100 hover:text-blue-400 ${
+              activeView === "Library" ? "bg-blue-100 text-blue-400" : ""
+            }`}
+          >
+            <Image src={library} alt="library" className="mr-2.5" />
             Library
           </button>
-          <button className="relative flex items-center justify-center xl:w-[193px] xl:h-[46px] xl:rounded-lg border">
+          <button
+            onClick={() => setActiveView("People")}
+            className={`relative flex items-center justify-center xl:w-[80%] xl:left-[10%] xl:h-[46px] xl:rounded-lg hover:bg-blue-100 hover:text-blue-400 ${
+              activeView === "People" ? "bg-blue-100 text-blue-400" : ""
+            }`}
+          >
+            <Image src={people} alt="people" className="mr-2.5" />
             People
           </button>
-          <button className="relative flex items-center justify-center xl:w-[193px] xl:h-[46px] xl:rounded-lg border">
+          <button
+            onClick={() => setActiveView("Activities")}
+            className={`relative flex items-center justify-center xl:w-[80%] xl:left-[10%] xl:h-[46px] xl:rounded-lg hover:bg-blue-100 hover:text-blue-400 ${
+              activeView === "Activities" ? "bg-blue-100 text-blue-400" : ""
+            }`}
+          >
+            <Image src={activities} alt="activities" className="mr-2.5" />
             Activities
           </button>
         </div>
         {/* end menu buttons */}
+
+        {/* support bar */}
+        <div className="relative flex flex-col xl:top-[140px] font-bold">
+          <h2 className="relative flex xl:left-[42px]">Support</h2>
+
+          <button className="relative flex xl:top-[20px] xl:rounded-lg hover:bg-blue-100 hover:text-blue-400 items-center justify-center xl:w-[80%] xl:left-[10%] xl:h-[46px]">
+            <Image src={getStarted} alt="get started" className="-ml-2" />
+            <p className="ml-4">Get started</p>
+          </button>
+          <button className="relative flex xl:top-[40px] xl:rounded-lg hover:bg-blue-100 hover:text-blue-400 items-center justify-center xl:w-[80%] xl:left-[10%] xl:h-[46px] ">
+            <Image src={settings} alt="settings" className="-ml-[28px]" />
+            <p className="ml-[16px]">Settings</p>
+          </button>
+        </div>
+        {/* end support bar */}
+
+        {/* current user */}
+        <div className="relative flex flex-col xl:top-[47%]">
+          <div className="relative flex xl:w-[90%] xl:h-[1px] bg-gray-300 xl:left-[10%]"></div>
+          <div className="relative flex flex-col xl:w-[80%] xl:left-[20%] xl:top-12">
+            <Image src={user} alt="current user" />
+            <h3 className="font-bold">Sam Wheeler</h3>
+            <p className="text-[10px]">samwheeler@example.com</p>
+          </div>
+        </div>
+        {/* end current user */}
       </div>
       {/* end side bar */}
 
       {/* main content */}
-      <div className="relative flex w-full">
-        {/* <Image src={rightContent} alt="main content" /> */}
-      </div>
+      <div className="relative flex w-full">{renderContent()}</div>
       {/* end main content */}
     </div>
   );
