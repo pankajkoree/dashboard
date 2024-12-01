564d3ea971f39219e5089a2a1e1af56252501cd4
pankajkoree <pankajkori2057@gmail.com>
2024-12-01 23:04:34 +0530

    dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -195,7 +195,7 @@ const Dashboard = () => {
             {/* topics */}
             <div className="relative grid grid-cols-2 xl:top-20 gap-4">
               {/* weakest */}
-              <div>
+              <div className="relative flex flex-col p-4">
                 <div>Weakest Topics</div>
 
                 <div>
@@ -232,11 +232,41 @@ const Dashboard = () => {
                 </div>
               </div>
               {/* strongest */}
-              <div>
+              <div className="relative flex flex-col p-4">
                 <div>Strongest Topics</div>
-                <div></div>
-                <div></div>
-                <div></div>
+
+                <div>
+                  <Image
+                    src={strong1}
+                    alt={data?.topics.strongest[0].name}
+                    width="100"
+                    height="100"
+                  />
+                  <p>{data?.topics.strongest[0].name}</p>
+                  <p>{data?.topics.strongest[0].correct_percentage}</p>
+                </div>
+
+                <div>
+                  <Image
+                    src={strong2}
+                    alt={data?.topics.strongest[1].name}
+                    width="100"
+                    height="100"
+                  />
+                  <p>{data?.topics.strongest[1].name}</p>
+                  <p>{data?.topics.strongest[1].correct_percentage}</p>
+                </div>
+
+                <div>
+                  <Image
+                    src={strong3}
+                    alt={data?.topics.strongest[2].name}
+                    width="100"
+                    height="100"
+                  />
+                  <p>{data?.topics.strongest[2].name}</p>
+                  <p>{data?.topics.strongest[2].correct_percentage}</p>
+                </div>
               </div>
             </div>
             {/* end topics */}
